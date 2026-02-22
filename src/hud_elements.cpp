535b4feef524f45c505da9cae6bbd4d47515ba9c
honjow <honjow311@gmail.com>
2026-02-22 09:39:45 +0800

    feat: display battery charging power
    - Display actual power value during battery charging
    - Add +/- prefix to power value to indicate charging/discharging status

--- src/hud_elements.cpp
+++ src/hud_elements.cpp
@@ -1250,13 +1250,19 @@ void HudElements::battery(){
             if (Battery_Stats.current_watt != 0) {
                 if (HUDElements.params->enabled[OVERLAY_PARAM_ENABLED_battery_watt]){
                     ImguiNextColumnOrNewRow();
+                    std::string prefix = "";
+                    if (Battery_Stats.current_status == "Charging") {
+                        prefix = "+"; //
+                    } else if (Battery_Stats.current_status == "Discharging") {
+                        prefix = "-"; //
+                    }
                     if (HUDElements.params->enabled[OVERLAY_PARAM_ENABLED_hud_compact] && Battery_Stats.current_watt >= 10.0f)
                         right_aligned_text(HUDElements.colors.text, HUDElements.ralign_width, "%.0f", Battery_Stats.current_watt);
                     else
                         right_aligned_text(HUDElements.colors.text, HUDElements.ralign_width, "%.1f", Battery_Stats.current_watt);
                     ImGui::SameLine(0,1.0f);
                     ImGui::PushFont(HUDElements.sw_stats->font_small);
-                    HUDElements.TextColored(HUDElements.colors.text, "W");
+                    HUDElements.TextColored(HUDElements.colors.text, "%sW", prefix.c_str());
                     ImGui::PopFont();
                 }
                 if (HUDElements.params->enabled[OVERLAY_PARAM_ENABLED_battery_time]) {
